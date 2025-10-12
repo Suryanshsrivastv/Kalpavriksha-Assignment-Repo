bd9b725c9e980bc5a818ab6ecf8b524164677cd2
Suryanshsrivastv <stanay924@gmail.com>
2025-10-13 00:58:26 +0530

    changed loop variables and added more robust input validation

--- Assignment2/StudentGradingSystem.c
+++ Assignment2/StudentGradingSystem.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 struct studentRecord{
-        int studentRollNumber;
-        char studentName[50];
-        int studentMarks[3];
+    int studentRollNumber;
+    char studentName[50];
+    float studentMarks[3];
 };
- 
-int getStudentotalMarks(int studentMarks[]){
-    int marks = 0;
-    for(int i=0;i<3;i++){
-        marks += studentMarks[i];
+
+float getStudentTotalMarks(float studentMarks[]){
+    float marks = 0.0f;
+    for(int subjectIndex = 0; subjectIndex < 3; subjectIndex++){
+        marks += studentMarks[subjectIndex];
     }
-    
     return marks;
 }
 
-void printStudentDetails(struct studentRecord student , float studentAverageMarks , int studentTotalMarks , char studentGrade){
-    printf(" Roll: %d \n Name: %s \n Total: %d \n Average: %.2f \n Grade %c \n ",
-    student.studentRollNumber,student.studentName,studentTotalMarks,studentAverageMarks,studentGrade);
+void printStudentDetails(struct studentRecord student, float studentAverageMarks, float studentTotalMarks, char studentGrade){
+    printf(" Roll: %d \n Name: %s \n Total: %.2f \n Average: %.2f \n Grade %c \n ",
+    student.studentRollNumber, student.studentName, studentTotalMarks, studentAverageMarks, studentGrade);
 }
 
 void printPerformancePattern(char studentGrade) {
@@ -48,68 +47,81 @@ char getStudentGrade(float averageMarks) {
     }
 }
 
-float calculateStudentAverageMarks(int studentsTotalMarks){
-    float averageMarks = studentsTotalMarks / 3.0;
-    return averageMarks;
+
+float calculateStudentAverageMarks(float studentsTotalMarks){
+    return studentsTotalMarks / 3.0f;
 }
 
-void printNumberRecursion(int index ,int numberStudents, struct studentRecord studentsArray[]){
-    if(index > numberStudents-1){
+void printNumberRecursion(int index, int numberStudents, struct studentRecord studentsArray[]){
+    if(index > numberStudents - 1){
         return;
     }
-    printf("%d ",studentsArray[index].studentRollNumber);
-    printNumberRecursion(index+1,numberStudents,studentsArray);
+    printf("%d ", studentsArray[index].studentRollNumber);
+    printNumberRecursion(index + 1, numberStudents, studentsArray);
 }
 
-int areMarksValid(int marks[]) {
-    for (int i = 0; i < 3; i++) {
-        if (marks[i] < 0 || marks[i] > 100) {
+
+int areMarksValid(float marks[]) {
+    for (int subjectIndex = 0; subjectIndex < 3; subjectIndex++) {
+        if (marks[subjectIndex] < 0 || marks[subjectIndex] > 100) {
             return 0;
         }
     }
     return 1;
 }
 
-int main()
-{   
+int main() {
     printf("Enter the number of students in range (1-100): ");
+    char lineBufferForInput[100];
     int numberStudents = 0;
-    scanf("%d",&numberStudents);
 
-    if (numberStudents < 1 || numberStudents > 100) {
-    fprintf(stderr, "Error: Number of students must be between 1 and 100.\n");
-    return 1;
+    fgets(lineBufferForInput, sizeof(lineBufferForInput), stdin);
+
+    if (sscanf(lineBufferForInput, "%d", &numberStudents) != 1) {
+        fprintf(stderr, "Error: Invalid input. Please enter a whole number for the number of students.\n");
+        return 1;
     }
 
-    getchar();
+    if (numberStudents < 1 || numberStudents > 100) {
+        fprintf(stderr, "Error: Number of students must be between 1 and 100.\n");
+        return 1;
+    }
     
     struct studentRecord studentsArray[numberStudents];
-    char lineBufferForInput[100];
     
-    for(int i=0;i<numberStudents;i++){
-        printf("Enter details for student %d (Roll Name Marks1(0-100) Marks2(0-100) Marks3(0-100)): ", i + 1);
-        fgets(lineBufferForInput , sizeof(lineBufferForInput) , stdin);
+    for(int studentIndex = 0; studentIndex < numberStudents; studentIndex++){
+        printf("Enter details for student %d (Roll Name Marks1(0-100) Marks2(0-100) Marks3(0-100)): ", studentIndex + 1);
+        fgets(lineBufferForInput, sizeof(lineBufferForInput), stdin);
         
-        sscanf(lineBufferForInput , "%d %s %d %d %d",
-                &studentsArray[i].studentRollNumber,
-                studentsArray[i].studentName,
-                &studentsArray[i].studentMarks[0],
-                &studentsArray[i].studentMarks[1],
-                &studentsArray[i].studentMarks[2]
-            );
+        int itemsScanned = sscanf(lineBufferForInput, "%d %s %f %f %f",
+               &studentsArray[studentIndex].studentRollNumber,
+               studentsArray[studentIndex].studentName,
+               &studentsArray[studentIndex].studentMarks[0],
+               &studentsArray[studentIndex].studentMarks[1],
+               &studentsArray[studentIndex].studentMarks[2]);
+        
+        if (itemsScanned != 5) {
+           fprintf(stderr, "Error: Invalid input format. Please use 'Roll Name Marks1 Marks2 Marks3'.\n");
+           return 1;
+        }
 
-        if (!areMarksValid(studentsArray[i].studentMarks)) {
+        if (studentsArray[studentIndex].studentRollNumber < 0) {
+            fprintf(stderr, "Error: Roll number cannot be negative.\n");
+            return 1;
+        }
+
+        if (!areMarksValid(studentsArray[studentIndex].studentMarks)) {
             fprintf(stderr, "Error: Marks for student %s (Roll %d) must be between 0 and 100.\n",
-                studentsArray[i].studentName, studentsArray[i].studentRollNumber);
-                return 1;
+                    studentsArray[studentIndex].studentName, studentsArray[studentIndex].studentRollNumber);
+            return 1;
         }
     }
     
-    for(int i=0;i<numberStudents;i++){
-        int studentsTotalMarks = getStudentotalMarks(studentsArray[i].studentMarks);
+    for(int studentIndex = 0; studentIndex < numberStudents; studentIndex++){
+        float studentsTotalMarks = getStudentTotalMarks(studentsArray[studentIndex].studentMarks);
         float studentAverageMarks = calculateStudentAverageMarks(studentsTotalMarks);
         char studentGrade = getStudentGrade(studentAverageMarks);
-        printStudentDetails(studentsArray[i] , studentAverageMarks ,  studentsTotalMarks , studentGrade);
+        printStudentDetails(studentsArray[studentIndex], studentAverageMarks, studentsTotalMarks, studentGrade);
         if(studentGrade != 'F'){
             printPerformancePattern(studentGrade);
         }
@@ -117,8 +129,7 @@ int main()
     }
     
     printf("\n Recursion for Roll Number of students :");
-    printNumberRecursion(0, numberStudents , studentsArray);
+    printNumberRecursion(0, numberStudents, studentsArray);
     
-
     return 0;
 }
